16092a4f77471d067efc62684f0176df2358c9e2
tkgstrator <nasawake.am@gmail.com>
2024-07-25 14:56:07 +0000

    feat(features): add logger, csrf, docs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import { Hono } from 'hono'
+import { OpenAPIHono as Hono } from '@hono/zod-openapi'
+import { apiReference } from '@scalar/hono-api-reference'
+import { csrf } from 'hono/csrf'
+import { logger } from 'hono/logger'
+import info from '../package.json'
+import { app as users } from './users'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use(csrf())
+app.use(logger())
+app.doc('/specification', {
+  openapi: '3.0.0',
+  info: {
+    title: info.name,
+    version: info.version,
+    license: {
+      name: info.license
+    }
+  }
 })
+app.get(
+  '/docs',
+  apiReference({
+    spec: {
+      url: '/specification'
+    }
+  })
+)
+app.route('/users', users)
 
 export default app
