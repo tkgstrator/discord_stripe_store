9be6f37b71b5088c4d74cde4496d463db7a92544
tkgstrator <nasawake.am@gmail.com>
2024-07-26 01:08:56 +0000

    feat: add products, users features

---
+++ src/products/index.ts
@@ -0,0 +1,41 @@
+import { HTTPMethod } from '@/enums/method'
+import { Product } from '@/schemas/product.dto'
+import type { Bindings } from '@/utils/bindings'
+import { OpenAPIHono as Hono, createRoute, z } from '@hono/zod-openapi'
+import Stripe from 'stripe'
+
+export const app = new Hono<{ Bindings: Bindings }>()
+
+app.openapi(
+  createRoute({
+    method: HTTPMethod.GET,
+    path: '/',
+    tags: ['Products'],
+    summary: 'List all products',
+    description:
+      'Returns a list of your products. The products are returned sorted by creation date, with the most recently created products appearing first.',
+    request: {
+      query: Product.Param
+    },
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: z.array(Product.Data)
+          }
+        },
+        description:
+          'A object with a data property that contains an array of up to limit products, starting after product starting_after. Each entry in the array is a separate product object. If no more products are available, the resulting array will be empty.'
+      }
+    }
+  }),
+  async (c) => {
+    const param = c.req.valid('query')
+    const stripe = new Stripe(c.env.STRIPE_SECRET_KEY, {
+      apiVersion: '2024-06-20',
+      typescript: true
+    })
+    const products = (await stripe.products.list({ limit: param.limit, active: true })).data.map((product) => Product.Data.parse(product))
+    return c.json(products, 200)
+  }
+)
--- src/users/index.ts
+++ src/users/index.ts
@@ -1,13 +1,18 @@
 import { HTTPMethod } from '@/enums/method'
+import { Mode } from '@/enums/mode'
 import { User } from '@/schemas/users.dto'
+import type { Bindings } from '@/utils/bindings'
 import { OpenAPIHono as Hono, createRoute, z } from '@hono/zod-openapi'
+import Stripe from 'stripe'
 
-export const app = new Hono()
+export const app = new Hono<{ Bindings: Bindings }>()
 
 app.openapi(
   createRoute({
     method: HTTPMethod.GET,
     path: '/',
+    tags: ['Users'],
+    summary: 'List all users',
     responses: {
       200: {
         content: {
@@ -15,16 +20,16 @@ app.openapi(
             schema: z.array(User.Data)
           }
         },
-        description: 'Retrieve users'
+        description: 'Find users'
       }
     }
   }),
-  (c) => {
+  async (c) => {
     return c.json(
       [
         {
           discord_user_id: '430364540899819520',
-          customer_id: '123',
+          customer_id: null,
           subscription: null
         }
       ],
@@ -36,6 +41,8 @@ app.openapi(
   createRoute({
     method: HTTPMethod.GET,
     path: '/{id}',
+    tags: ['Users'],
+    summary: 'Retrieve a user',
     request: {
       params: User.Param
     },
@@ -46,16 +53,112 @@ app.openapi(
             schema: User.Data
           }
         },
-        description: 'Retrieve the user'
+        description: 'Find a user'
+      },
+      404: {
+        description: 'User not found'
       }
     }
   }),
-  (c) => {
-    const { id } = c.req.valid('param')
+  async (c) => {
+    const param = c.req.valid('param')
     return c.json(
       {
-        discord_user_id: id,
-        customer_id: '123',
+        discord_user_id: param.id.toString(),
+        customer_id: null,
+        subscription: null
+      },
+      200
+    )
+  }
+)
+app.openapi(
+  createRoute({
+    method: HTTPMethod.POST,
+    path: '/',
+    tags: ['Users'],
+    summary: 'Create a user',
+    request: {
+      body: {
+        content: {
+          'application/json': {
+            schema: User.Param
+          }
+        },
+        required: true
+      }
+    },
+    responses: {
+      201: {
+        content: {
+          'application/json': {
+            schema: User.Data
+          }
+        },
+        description: 'Create a user'
+      },
+      400: {
+        description: 'Bad request'
+      }
+    }
+  }),
+  async (c) => {
+    const body = c.req.valid('json')
+    return c.json(
+      {
+        discord_user_id: body.id.toString(),
+        customer_id: null,
+        subscription: null
+      },
+      201
+    )
+  }
+)
+app.openapi(
+  createRoute({
+    method: HTTPMethod.GET,
+    path: '/{id}/checkout',
+    tags: ['Users'],
+    summary: 'Create a session',
+    description: 'Create a session for a user to checkout',
+    request: {
+      params: User.Checkout.Param
+    },
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: User.Data
+          }
+        },
+        description: 'Retrieve urls to checkout'
+      },
+      404: {
+        description: 'User not found'
+      }
+    }
+  }),
+  async (c) => {
+    const param = c.req.valid('param')
+    const stripe = new Stripe(c.env.STRIPE_SECRET_KEY, {
+      apiVersion: '2024-06-20',
+      typescript: true
+    })
+    const products = await stripe.products.list({ limit: 100, active: true })
+    const sessions = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price: products.data[0].id,
+          quantity: 1
+        }
+      ],
+      client_reference_id: param.id.toString(),
+      mode: Mode.SUBSCRIPTION
+    })
+    return c.json(
+      {
+        discord_user_id: '430364540899819520',
+        customer_id: null,
         subscription: null
       },
       200
