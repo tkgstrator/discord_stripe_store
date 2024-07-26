75a8357f1eb9555dcfe0cbf45a780a602788890b
tkgstrator <nasawake.am@gmail.com>
2024-07-26 11:40:07 +0900

    refactor: fix codes (#7)
    * ci: change deploy workflows
    
    * refactor: add validation of limit
    
    * refactor: export openapi configuration

--- src/index.ts
+++ src/index.ts
@@ -2,45 +2,16 @@ import { OpenAPIHono as Hono } from '@hono/zod-openapi'
 import { apiReference } from '@scalar/hono-api-reference'
 import { csrf } from 'hono/csrf'
 import { logger } from 'hono/logger'
-import info from '../package.json'
 import { app as products } from './products'
 import { app as users } from './users'
+import { reference, specification } from './utils/openapi'
 
 const app = new Hono()
 
 app.use(csrf())
 app.use(logger())
-app.doc('/specification', {
-  openapi: '3.0.0',
-  info: {
-    title: info.name,
-    version: info.version,
-    license: {
-      name: info.license
-    }
-  }
-})
-app.get(
-  '/docs',
-  apiReference({
-    spec: {
-      url: '/specification'
-    },
-    defaultHttpClient: {
-      targetKey: 'node',
-      clientKey: 'axios'
-    },
-    layout: 'modern',
-    hideDownloadButton: true,
-    darkMode: true,
-    metaData: {
-      title: info.name
-    },
-    theme: 'bluePlanet',
-    defaultOpenAllTags: false,
-    tagsSorter: 'alpha'
-  })
-)
+app.doc('/specification', specification)
+app.get('/docs', apiReference(reference))
 app.route('/users', users)
 app.route('/products', products)
 
