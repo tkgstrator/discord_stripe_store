28fd7705f41e15a85b840a7bc7c30d5d441184ad
tkgstrator <tkgstrator@users.noreply.github.com>
2025-03-07 19:28:04 +0000

    chore(gitignore): add terraform.tfvars to .gitignore

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.0"
+      version = "~> 4.51"
     }
   }
 }
 
 provider "cloudflare" {
-  api_token = var.cloudflare_api_token
+  api_token = var.api_token
 }
